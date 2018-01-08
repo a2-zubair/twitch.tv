4824bc2dd3c2600f39ad8c9b8952afb6ae31dca9
Abdullah-Al-Zubair <remember.zahid@gmail.com>
2018-01-09 01:53:06 +0600

    none

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-    
+    var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+    // api url
+    var url = ;
+
+    $.ajax({
+        type: "GET",
+        url: url,
+        dataType: json,
+        async: false,
+        success:function(data){
+
+        },
+        cache:false,
+        error:function(errorMessage){}
+    });
+
+    // api url generator
+    function apiUrlGenerator(){
+        
+    }
 });
